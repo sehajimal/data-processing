f4a5a6a20998ae9c31b9c41f6477b965ccfe1a8e
Sehaj-10 <sehajimal@gmail.com>
2024-11-21 13:25:28 -0500

    done

--- clean.c
+++ clean.c
@@ -1,19 +1,37 @@
+/* Sehaj Ajimal, 400511220, 2024 
+ * SE 2XC3 Data Processing Assignment
+ * 
+ * The purpose is to process a 2D array by reading the data and cleaning it 
+ * We can get rid of nan values by finding the average of the values in the column or by completley deleting the row.
+ * Each function handles different aspects by reading, computing and outputting the array
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include "clean.h"
 
+/* Main
+ * 
+ * The purpose of the main is to run the functions in a certain order to output the 2Darray.
+ * if -d flag is used then I use the deletion strategy otherwise the imputation strategy
+ *
+ * Parameters: argc: The number of command-line arguments. argv: An array of command-line arguments.
+ *
+ * Return value: EXIT_SUCCESS on successful execution.
+ */
 int main(int argc, char *argv[])
 {
     int rows;
     int columns;
 
-    float *array = read_data(&rows, &columns);          //calling in order to create 2Darray
-
-    float (*array2D)[columns] = (float (*)[columns])array;      //cast it to the correct type ( float (*)[cols]) 
+    // Read and store the data into a 2D array
+    float *array = read_data(&rows, &columns);          
+    float (*array2D)[columns] = (float (*)[columns])array; // Cast to the correct 2D type
 
     if (argc == 1) 
     {
+        // Perform imputation strategy
         float *array1 = clean_impute(&rows, &columns, array2D);
         float (*array1_2D)[columns] = (float (*)[columns])array1;
         output_data(&rows, &columns, array1_2D);
@@ -22,6 +40,7 @@ int main(int argc, char *argv[])
 
     if (argc == 2 && strcmp(argv[1], "-d") == 0) 
     {
+        // Deletion strategy
         float *array1 = clean_delete(&rows, &columns, array2D);
         float (*array1_2D)[columns] = (float (*)[columns])array1;
         output_data(&rows, &columns, array1_2D);
--- clean2.c
+++ clean2.c
@@ -1,23 +1,41 @@
+/* Sehaj Ajimal, 400511220, 2024
+ * Second .c file which is used to make the code more modular and contains all of the functions outside of main
+ * These are the functions specified in the question to create an array and process it in two different ways
+ * Then the array is outputted as specified.
+ **/
+
 #include <stdbool.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
 
+/* 
+ * read_data
+ * 
+ * Reads the rows and columns and number of elements of a 2D array from standard input.
+ *
+ * Parameters: rows: Pointer to store the number of rows. columns: Pointer to store the number of columns.
+ *
+ * Return value: Pointer to the dynamically allocated array containing the input data.
+ */
 float *read_data(int *rows, int *columns)
 {
+
     scanf("%d %d", rows, columns);
     long elements = *rows * *columns;
 
+    // Allocate memory for the 2D array
     float (*p)[*columns] = malloc(elements * sizeof(float));
     
     if (p == NULL)
     {
-        return NULL;
+        return NULL; // Return NULL if memory allocation fails
     } 
 
-    for (int i=0; i<*rows; i++) 
+    // Populate the array with input data
+    for (int i = 0; i < *rows; i++) 
     {
-        for (int j=0; j<*columns; j++) 
+        for (int j = 0; j < *columns; j++) 
         {
             scanf("%f", &p[i][j]);
         }
@@ -25,59 +43,83 @@ float *read_data(int *rows, int *columns)
     return *p;
 }
 
+/* 
+ * clean_delete
+ *
+ * The purpose is to remove the rows containing nan from the 2D array.
+ *
+ * Parameters: rows: Pointer to the number of rows, updated after deletion. columns: Pointer to the number of columns. array2D: Pointer to the 2D array to process.
+ *
+ * Return value: Pointer to the new 2D array with rows containing nan values removed.
+ */
 float *clean_delete(int *rows, int *columns, float (*array2D)[*columns])
 {
-    float (*arraycopy)[*columns] = malloc(*rows**columns * sizeof(float));
+    // Allocate memory for the new array
+    float (*arraycopy)[*columns] = malloc(*rows * *columns * sizeof(float));
     int newRows = 0;
 
     if (arraycopy == NULL)
     {
-        return NULL;
+        return NULL; // Return NULL if memory allocation fails
     }
 
-    for (int i=0; i<*rows; i++) 
+    // Process rows and exclude those with NaN values
+    for (int i = 0; i < *rows; i++) 
     {
         int remove = 0;
-        for (int j=0; j<*columns; j++) 
+        for (int j = 0; j < *columns; j++) 
         {
             if (isnan(array2D[i][j]))
             {
-                remove = 1;
+                remove = 1; // Mark row for removal if NaN is found
                 break;
             }
         }
 
         if (remove == 0)
         {                
-            for (int j=0; j<*columns; j++) 
+            // Copy the row to the new array
+            for (int j = 0; j < *columns; j++) 
             {
                 arraycopy[newRows][j] = array2D[i][j];
             }
-            newRows = newRows + 1;
+            newRows++;
         }
     }
+
     *rows = newRows;
-    arraycopy = realloc(arraycopy, newRows* *columns *sizeof(float));
+    arraycopy = realloc(arraycopy, newRows * *columns * sizeof(float)); // Resize memory
     return *arraycopy;
 }
 
+/* 
+ * clean_impute
+ *
+ * The purpose is to replace the nan with the average of the columns in the 2D array.
+ *
+ * Parameters: rows: Pointer to the number of rows. columns: Pointer to the number of columns. array2D: Pointer to the 2D array to process.
+ *
+ * Return value: Pointer to the modified 2D array with NaN values replaced.
+ */
 float *clean_impute(int *rows, int *columns, float (*array2D)[*columns])
 {
-    for (int j=0; j<*columns; j++) 
+    for (int j = 0; j < *columns; j++) 
     {
         float total = 0;
         int count = 0;
         float average;
-        for (int i=0; i<*rows; i++) 
+
+        // Calculate column average
+        for (int i = 0; i < *rows; i++) 
         {
-            if (!(isnan(array2D[i][j])))
+            if (!isnan(array2D[i][j]))
             {
                 total += array2D[i][j];
-                count = count + 1;
+                count++;
             }
         }
 
-        if (count > 0)
+         if (count > 0)
         {
             average = (total /count);
         }
@@ -87,9 +129,10 @@ float *clean_impute(int *rows, int *columns, float (*array2D)[*columns])
             average = 0;
         }
 
-        for (int i=0; i<*rows; i++) 
+        // Replace NaN values with the column average
+        for (int i = 0; i < *rows; i++) 
         {
-            if ((isnan(array2D[i][j])))
+            if (isnan(array2D[i][j]))
             {
                 array2D[i][j] = average;
             }
@@ -98,16 +141,23 @@ float *clean_impute(int *rows, int *columns, float (*array2D)[*columns])
     return *array2D;
 }
 
+/* 
+ * output_data
+ *
+ * The purpose is to print the 2D array as specified
+ *
+ * Parameters: rows: Pointer to the number of rows. columns: Pointer to the number of columns. array2D: Pointer to the 2D array to output.
+ */
 void output_data(int *rows, int *columns, float (*array2D)[*columns]) 
 {
     printf("%d %d\n", *rows, *columns);
 
-    for (int r = 0; r < *rows; r++) {
-        for (int c = 0; c < *columns; c++) {
-            printf("%.3f ", array2D[r][c]);
+    for (int r = 0; r < *rows; r++) 
+    {
+        for (int c = 0; c < *columns; c++) 
+        {
+            printf("%.3f ", array2D[r][c]); // Print each element with 3 decimal precision
         }
         puts("");
     }
 }
-
-
